7626b372b1b6e3136329b4685b70079cf0467d01
Scubik1 <138704511+Scubik1@users.noreply.github.com>
2023-07-21 14:16:46 +0300

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,50 @@
+node slave1.puppet {
+  package { 'httpd':
+    ensure => installed,
+  }
+  file { '/var/www/html/index.html':
+    ensure => present,
+    source => "/vagrant/index.html",
+  }
+  service { 'httpd':
+    ensure => running,
+    enable => true,
+  }
+  exec {'Add port to firewall':
+    path => '/usr/bin',
+    command => 'firewall-cmd --add-port=80/tcp --permanent',
+  }
+  exec { 'Restart firewall':
+    command   => '/usr/bin/systemctl restart firewalld',
+    path      => '/usr/bin',
+  }
+}
+
+node slave2.puppet {
+  package { 'httpd':
+    ensure => installed,
+  }
+  package { 'php':
+    ensure => installed,
+  }
+  file { '/var/www/html/index.php':
+    ensure => present,
+    source => "/vagrant/index.php",
+  }
+#  service { 'php-fpm':
+ #   ensure => running,
+  #  enable => true,
+  #}
+  service { 'httpd':
+    ensure => running,
+    enable => true,
+  }
+  exec {'Add port to firewall':
+    path => '/usr/bin',
+    command => 'firewall-cmd --add-port=80/tcp --permanent',
+  }
+  exec { 'Restart firewall':
+    command   => '/usr/bin/systemctl restart firewalld',
+    path      => '/usr/bin',
+  }
+}
