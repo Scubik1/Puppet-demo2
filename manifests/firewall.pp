5e761d18691a6a1bbc81d1565c3cb3f8ed142ece
Scubik1 <138704511+Scubik1@users.noreply.github.com>
2023-07-22 22:41:27 +0300

    Update firewall.pp

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -1,10 +1,19 @@
   class { 'firewalld': }
   
-  class firewall_node1 {
-    firewalld_port { 'Open 80 and 81 ports':
+  class firewall_open_port_80 {
+    firewalld_port { 'Open port 80':
       ensure   => present,
       zone     => 'public',
       port     => 80,
+      protocol => 'tcp',
+    }
+  }
+
+  class firewall_open_port_81 {
+    firewalld_port { 'Open port 81':
+      ensure   => present,
+      zone     => 'public',
+      port     => 81,
       protocol => 'tcp',
     }
   }
