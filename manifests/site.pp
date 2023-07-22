765571cec27763abae35484de18d70c9fac5b5ca
Scubik1 <138704511+Scubik1@users.noreply.github.com>
2023-07-22 21:13:16 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,25 @@
+node master.puppet {
+  include nginx
+  nginx::resource::server { 'Redirect to slave1.puppet':
+    listen_port => 80,
+    proxy       => 'http://192.168.50.10',
+  }
+  nginx::resource::server { 'Redirect to slave2.puppet':
+    listen_port => 81,
+    proxy       => 'http://192.168.50.15',
+  }
+  class { 'firewalld': }
+  class firewall_node1 {
+    firewalld_port { 'Open 80 and 81 ports':
+      ensure   => present,
+      zone     => 'public',
+      port     => 80,
+      protocol => 'tcp',
+    }
+  }
+}
+
+
 node slave1.puppet {
   package { 'httpd':
     ensure => installed,
@@ -11,12 +33,12 @@ node slave1.puppet {
     enable => true,
   }
   exec {'Add port to firewall':
-    path => '/usr/bin',
+    path    => '/usr/bin',
     command => 'firewall-cmd --add-port=80/tcp --permanent',
   }
   exec { 'Restart firewall':
-    command   => '/usr/bin/systemctl restart firewalld',
-    path      => '/usr/bin',
+    command => '/usr/bin/systemctl restart firewalld',
+    path    => '/usr/bin',
   }
 }
 
@@ -40,17 +62,11 @@ node slave2.puppet {
     enable => true,
   }
   exec {'Add port to firewall':
-    path => '/usr/bin',
+    path    => '/usr/bin',
     command => 'firewall-cmd --add-port=80/tcp --permanent',
   }
   exec { 'Restart firewall':
-    command   => '/usr/bin/systemctl restart firewalld',
-    path      => '/usr/bin',
-  }
-}
-
-node master.puppet {
-  package { 'mc':
-    ensure => installed,
+    command => '/usr/bin/systemctl restart firewalld',
+    path    => '/usr/bin',
   }
 }
