ed2d69452762bbbda5b1447657b52a90773282f1
Scubik1 <138704511+Scubik1@users.noreply.github.com>
2023-07-22 23:58:35 +0300

    Update init.pp

--- modules/minecraft/manifests/init.pp
+++ modules/minecraft/manifests/init.pp
@@ -1,3 +1,29 @@
 class minecraft {
-
+  package {  'java':
+    name   => java-17-openjdk,
+    ensure => present,
+  }
+  file { '/opt/minecraft':
+    ensure => directory,
+  }
+  wget::retrieve { 'Download minecraft server':
+    source      => 'https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar',
+    destination => '/opt/minecraft/',
+    timeout     => 0,
+    verbose     => false,
+    require => File['/opt/minecraft'],  
+  }
+  file { '/opt/minecraft/eula.txt':
+    content => "eula=true",
+    require => Exec['init start server'],
+  }  
+  file { '/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+  service { 'minecraft.service':
+    ensure => running,
+    enable => true,
+    require => File['/etc/systemd/system/minecraft.service'],
+  }
 }
